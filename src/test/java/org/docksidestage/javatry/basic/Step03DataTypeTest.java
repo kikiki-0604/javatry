39642663280be4a1552866c58c1d37283d1141ee
FTS-B-0137-NBP\hikaru.kinami <kinami0604@gmail.com>
2024-01-24 18:23:07 +0900

    testComit

--- src/test/java/org/docksidestage/javatry/basic/Step03DataTypeTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step03DataTypeTest.java
@@ -54,7 +54,9 @@ public class Step03DataTypeTest extends PlainTestCase {
             BigDecimal addedDecimal = amba.add(new BigDecimal(land));
             sea = String.valueOf(addedDecimal);
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 18.4
+
+        assertEquals("18.4",sea);
     }
 
     // ===================================================================================
@@ -82,7 +84,9 @@ public class Step03DataTypeTest extends PlainTestCase {
         if (dstore > piari) {
             sea = 0;
         }
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 2
+
+        assertEquals(2,sea);
     }
 
     // ===================================================================================
@@ -92,7 +96,10 @@ public class Step03DataTypeTest extends PlainTestCase {
     public void test_datatype_object() {
         St3ImmutableStage stage = new St3ImmutableStage("hangar");
         String sea = stage.getStageName();
-        log(sea); // your answer? => 
+        log(sea); // your answer? => hangar
+        log(stage + "aaaa");
+
+        assertEquals("hangar",sea);
     }
 
     private static class St3ImmutableStage {
